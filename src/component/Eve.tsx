e33895a0801af631410daf47528bf54b5f3f22bd
krisk22 <7heblackink@gmail.com>
2022-03-04 13:48:26 -0500

    logo

--- src/component/Eve.tsx
+++ src/component/Eve.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Socials from './Socials';
 import styles from '../css/Eve.module.scss';
+import logo from "../assets/LOGO.png"
 
 function Eve({ description, cta }: {description?: React.ReactNode, cta: React.ReactNode}) {
   return (
     <div className={styles.container}>
       <img
         className={styles.image}
-        src="LOGO.png"
+        src={logo}
         alt="chippies nft"
       />
       <div className={styles.description}>
